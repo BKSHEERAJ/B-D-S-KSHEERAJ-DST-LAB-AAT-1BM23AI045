415e775920a000a8a28523197fd8fec0d67a6eb8
BKSHEERAJ <153779404+BKSHEERAJ@users.noreply.github.com>
2025-01-13 18:25:56 +0530

    Update Queue using Two Stacks.py

--- Queue using Two Stacks.py
+++ Queue using Two Stacks.py
@@ -1,9 +1,9 @@
 class QueueUsingTwoStacks:
     def __init__(self):
-        self.stack1 = []
-        self.stack2 = []
+        self.stack1=[]
+        self.stack2=[]
 
-    def enqueue(self, value):
+    def enqueue(self,value):
         self.stack1.append(value)
 
     def dequeue(self):
@@ -20,14 +20,14 @@ class QueueUsingTwoStacks:
         if self.stack2:
             return self.stack2[-1]
 
-if __name__ == "__main__":
-    q = int(input().strip())
-    queue = QueueUsingTwoStacks()
+if __name__=="__main__":
+    q=int(input().strip())
+    queue=QueueUsingTwoStacks()
     for _ in range(q):
-        query = input().strip().split()
-        if query[0] == "1":
+        query=input().strip().split()
+        if query[0]=="1":
             queue.enqueue(int(query[1]))
-        elif query[0] == "2":
+        elif query[0]=="2":
             queue.dequeue()
-        elif query[0] == "3":
+        elif query[0]=="3":
             print(queue.peek())
