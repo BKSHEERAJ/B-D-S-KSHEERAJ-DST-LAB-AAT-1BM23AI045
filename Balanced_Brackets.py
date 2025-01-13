16b513fae9cede81805de0c92eedaa5bce07307b
BKSHEERAJ <153779404+BKSHEERAJ@users.noreply.github.com>
2025-01-13 18:17:34 +0530

    Update Balanced_Brackets.py

--- Balanced_Brackets.py
+++ Balanced_Brackets.py
@@ -1,19 +1,19 @@
 def isBalanced(s):
-    stack = []
-    pairs = {')': '(', '}': '{', ']': '['}
+    stack=[]
+    pairs={')':'(','}':'{',']':'['}
     for char in s:
         if char in pairs.values():
             stack.append(char)
         elif char in pairs.keys():
-            if not stack or stack.pop() != pairs[char]:
+            if not stack or stack.pop()!=pairs[char]:
                 return "NO"
     return "YES" if not stack else "NO"
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-    t = int(input().strip())
+if __name__=='__main__':
+    fptr=open(os.environ['OUTPUT_PATH'],'w')
+    t=int(input().strip())
     for _ in range(t):
-        s = input()
-        result = isBalanced(s)
-        fptr.write(result + '\n')
+        s=input()
+        result=isBalanced(s)
+        fptr.write(result+'\n')
     fptr.close()
